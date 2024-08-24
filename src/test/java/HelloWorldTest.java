df788a1a7e8adf87fdc4dadbb13dce0b13b4e501
IMorzeeva <inna_001@mail.ru>
2024-08-24 12:49:46 +0300

    the 3.rd task

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,21 +4,6 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
-
-//    @Test
-//    public void testHelloWorld(){
-//       Response response = RestAssured
-//               .get("https://playground.learnqa.ru/api/hello")
-//        .andReturn();
-//       response.prettyPrint();
-//    }
-
-
-//    @Test
-//    public void testHelloWorld(){
-//        System.out.println("Hello from Inna");
-//    }
-
         @Test
     public void testHelloWorld(){
        Response response = RestAssured
